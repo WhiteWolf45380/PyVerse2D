0fb1c13b30eb97ea43734810b30046333d8e642a
WhiteWolf <matheodeponte45@gmail.com>
2026-03-11 14:59:32 +0100

    chore: pyi doc

---
+++ engine/shape/__init__.pyi
@@ -0,0 +1,10 @@
+# ======================================== IMPORTS ========================================
+from .capsule import Capsule
+from .circle import Circle
+from .ellipse import Ellipse
+from .polygon import Polygon
+from .rect import Rect
+from .segment import Segment
+
+# ======================================== EXPORTS ========================================
+__all__: list
