993f5d2073a26cf9ac19bb0914250e4d9be600ef
WhiteWolf45380 <matheodeponte45@gmail.com>
2026-05-01 11:50:57 +0200

    fixe: syntax in CollisionSystem._registry

--- pyverse2d/world/_system/_collision/_registry.py
+++ pyverse2d/world/_system/_collision/_registry.py
@@ -1,14 +1,11 @@
 # ======================================== IMPORTS ========================================
 from __future__ import annotations
 
+from ...._core import Geometry
 from ....math import Vector
-
 from ....abc import Shape
 
-from ..._component import Transform
-
 from typing import Callable, NamedTuple, Type
-from math import radians, cos, sin
 
 # ======================================== CONTACT ========================================
 class Contact(NamedTuple):
@@ -30,7 +27,7 @@ def register(type_a: Type[Shape], type_b: Type[Shape]):
     return decorator
 
 
-def dispatch(sa: Shape, ax: float, ay: float, scale_a: float, rot_a: float, sb: Shape, bx: float, by: float, scale_b: float, rot_b: float) -> Contact | None:
+def dispatch(geom_a: Geometry, geom_b: Geometry) -> Contact | None:
     """Dispatche vers le bon handler de narrowphase"""
     from ._narrow_phase import dispatch as _np_dispatch
-    return _np_dispatch(sa, ax, ay, scale_a, rot_a, sb, bx, by, scale_b, rot_b)
+    return _np_dispatch(geom_a, geom_b)
