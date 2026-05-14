c903cb8f727726002d84a0ece04960dd50913c1e
WhiteWolf45380 <matheodeponte45@gmail.com>
2026-05-14 23:23:38 +0200

    chore: bump to version 0.77.10

--- pyverse2d/math/easing.py
+++ pyverse2d/math/easing.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from ..typing import EasingFunc
 
 import numpy as np
-from typing import Callable, TypeAlias
+from typing import Callable
 
 # ======================================== TAG ========================================
 _EASING_TAG: object = object()
